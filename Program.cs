78c208c16651316ba56009e14f3dfe67b018ccbe
im4i4a <al.zemskov03@yandex.ru>
2025-09-30 23:41:56 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ConsoleAppSkillfactory
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
