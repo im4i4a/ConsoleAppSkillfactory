26cdaaa15b6a4bb7fb0c11d028dd672d1d481743
im4i4a <al.zemskov03@yandex.ru>
2025-10-01 13:58:43 +0300

    Разобрался с рекурсией

--- Program.cs
+++ Program.cs
@@ -1,10 +1,55 @@
-﻿namespace ConsoleAppSkillfactory
+﻿
+namespace ConsoleAppSkillfactory
 {
     internal class Program
     {
+       
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+   
+            Console.WriteLine(Factorial(20));
+
+
+            Console.WriteLine("Напишите что-то");
+            var str = Console.ReadLine();
+
+            Console.WriteLine("Укажите глубину эха");
+            var deep = int.Parse(Console.ReadLine());
+
+            Echo(str, deep);
+
+            Console.ReadKey();
+        }
+
+        static void Echo(string str, int deep)
+        {
+            string modif = str;
+            if(modif.Length > 2)
+            {
+                modif = modif.Remove(0, 2);
+            }
+
+            Console.BackgroundColor = (ConsoleColor)deep;
+            Console.WriteLine("..." + modif);
+            
+            
+            if (deep > 1) {
+                Echo(modif, deep - 1); 
+            }
+        }
+        static int Factorial(int n)
+        {
+            if (n <= 1)
+            {
+                return n;
+            }
+            else
+            {
+                return n * Factorial(n - 1);
+            }
+
+
+
         }
     }
 }
